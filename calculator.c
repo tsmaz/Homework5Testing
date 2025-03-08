533f6fbe31408052e494b31871d22bd71c45c953
Tony Mazich <tsmazich@cpp.edu>
2025-03-07 20:49:44 -0800

    Implemented multiply and divide functions

--- calculator.c
+++ calculator.c
@@ -9,3 +9,18 @@ int subtract(int a, int b)
 {
     return a - b;
 }
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    
+    return a / b;
+}
